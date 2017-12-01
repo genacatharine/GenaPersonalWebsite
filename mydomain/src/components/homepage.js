e6e8003368163f447871c6e58e458505bcf80c78
Gena Catherine <gena.catharine@gmail.com>
2017-11-30 20:40:18 -0700

    need to finish footer and put icons on horizontal line

---
+++ mydomain/src/components/homepage.js
@@ -0,0 +1,78 @@
+import React from 'react'
+
+const Index = () => {
+<div className="home">
+  {/* <head>
+    <meta charset="utf-8">
+    <title>Gena Israel</title> */}
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/main.css">
+    <script src="https://use.fontawesome.com/bc265e5e64.js"></script>
+    {/* <style>
+@import url('https://fonts.googleapis.com/css?family=Nunito');
+</style> */}
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+{/* Navbar */}
+<nav className="navbar navbar-default">
+  <div className="container-fluid">
+    <div className="navbar-header">
+      <a className="navbar-brand" href="#">Gena Israel
+      </a>
+    </div>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="nav navbar-nav navbar-right">
+        <li className="nav-item">
+          <a className="nav-link" href="#about" target="_blank">About</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#contact">Contact</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#portfolio" target="_blank">Portfolio</a>
+        </li>
+      </ul
+   </div>
+ </div>
+</nav>
+<!-- <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="http://www.genaisrael.com">Gena Israel</a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item">
+        <a className="nav-link" href="#contact">Contact</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#portfolio" target="_blank">Portfolio</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#" target="_blank">Resume</a>
+      </li>
+    </ul>
+  </div>
+</nav> -->
+<div className="social">
+  <h1>Social Media</h1>
+  <a href="https://github.com/genacatharine" target="_blank"><i className="fa fa-github" aria-hidden="true"></i></a>
+  <a className="nav-link" href="https://twitter.com/gena_catharine" target="_blank"><i className="fa fa-twitter" aria-hidden="true"></i></a>
+  <a className="nav-link" href="https://www.linkedin.com/in/genaisrael/" target="_blank"><i className="fa fa-linkedin" aria-hidden="true"></i></a>
+</div>
+{/* footer */}
+
+<footer className="container-fluid text-center footer">
+  <div className="footer-copyright">
+  <p>Made with <i className="fa fa-heart"></i>, Bootstrap and ReactJS. </p>
+  <p>&#9400; 2017</p>
+  <a href="#top">Back to top</a>
+</div>
+</footer>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  </body>
+</div>
+}
