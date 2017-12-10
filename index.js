61dbd5238372820a14e8bd4778c14bcb49fd553f
Gena Catherine <gena.catharine@gmail.com>
2017-12-10 09:48:21 -0700

    added javascript

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
-$(document).ready(function(){
-}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+      for (var i = 0; i < 156; i++) {
+      // create pixel divs
+      var element = document.createElement("div");
+      // add class pixel to div
+      element.classList.add("pixel");
+      // locate the pixel container
+      var pixelContainer= document.getElementById("pixelContainer");
+      pixelContainer.appendChild(element);
+      }
+      var currentColor="";
+      var sidebar= document.getElementById("sidebar");
+
+  // Grab color and reassign current color
+      sidebar.addEventListener('click', function(event){
+      currentColor = window.getComputedStyle(event.target).backgroundColor;
+
+  // Putting picked color onto pixel
+  var pixelContainer1=document.getElementById("pixelContainer")
+
+  pixelContainer1.addEventListener('click', function(event){
+    event.target.style.backgroundColor= currentColor;
+
+  })
+
+   });
+//   sidebar.addEventListener('click', function(event){
+//   currentColor= window.getComputedStyle(event.target).backgroundColor;
+//   console.log(currentColor)
+// })
+});
